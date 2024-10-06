87a64b6039c98123f8221e61c14b935c6fbdd3eb
Sylvien Scotland <169065373+SylvienScotland@users.noreply.github.com>
2024-10-06 17:48:15 -0400

    Add files via upload

--- Fibonacci sequence/src/Fibonacci.java
+++ Fibonacci sequence/src/Fibonacci.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 /**
  * A class that provides methods for calculating Fibonacci sequence terms.
  */
 public class Fibonacci {
 
     /**
-     * Computes the nth term in the Fibonacci sequence using recursion.
+     * Computes the nth term in the Fibonacci sequence using an iterative approach.
      *
      * @param n the index of the term in the Fibonacci sequence to compute
      * @return the nth term in the Fibonacci sequence
@@ -13,17 +15,27 @@ public class Fibonacci {
         if (n <= 1) {
             return n;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        int prev1 = 0, prev2 = 1, current = 1;
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return current;
     }
 
     /**
      * Main method to demonstrate the Fibonacci sequence calculation.
-     * It calculates and prints the 10th term in the Fibonacci sequence.
+     * It calculates and prints the nth term in the Fibonacci sequence based on user input.
      *
      * @param args command-line arguments (not used in this method)
      */
     public static void main(String[] args) {
-        int n = 10;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the term number (n) of the Fibonacci sequence you want to calculate: ");
+        int n = scanner.nextInt();
+
         int result = fibonacci(n);
         System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
     }
